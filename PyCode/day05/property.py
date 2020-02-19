1ce650156b405e464404bb21addb542e83799525
sunyf <portediSunyf@outlook.com>
2020-02-19 20:22:47 +0800

    property

---
+++ PyCode/day05/property.py
@@ -0,0 +1,52 @@
+'''
+
+
+'''
+
+# 在绑定属性时，如果我们直接把属性暴露出去，虽然写起来很简单，但是，没办法检查参数，导致可以把成绩随便改：
+# s = Student()
+# s.score = 9999
+
+# 这显然不合逻辑。为了限制score的范围，可以通过一个set_score()方法来设置成绩，再通过一个get_score()来获取成绩，这样，在set_score()方法里，就可以检查参数：
+# class Student(object):
+#
+#     def get_score(self):
+#          return self._score
+#
+#     def set_score(self, value):
+#         if not isinstance(value, int):
+#             raise ValueError('score must be an integer!')
+#         if value < 0 or value > 100:
+#             raise ValueError('score must between 0 ~ 100!')
+#         self._score = value
+#
+#
+#
+# s = Student()
+# s.set_score(20)
+# print(s.get_score())
+
+
+# 还记得装饰器（decorator）可以给函数动态加上功能吗？对于类的方法，装饰器一样起作用。Python内置的@property装饰器就是负责把一个方法变成属性调用的：
+
+
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+s = Student()
+s.score = 50
+print(s.score)
+
+
+# @property广泛应用在类的定义中，可以让调用者写出简短的代码，同时保证对参数进行必要的检查，这样，程序运行时就减少了出错的可能性。
